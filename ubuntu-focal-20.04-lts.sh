f3ecafb52c1eeb571b35812b64ebac21580a3213
Artur Zdolinski <artur@zdolinski.com>
2023-11-09 14:00:44 +0100

    Update ubuntu-focal-20.04-lts.sh

--- ubuntu-focal-20.04-lts.sh
+++ ubuntu-focal-20.04-lts.sh
@@ -26,7 +26,7 @@ if [ -e /etc/os-release ]; then
         else
             echo "Repository line already exists"
         fi
-        apk update && apk add nano wget curl libguestfs p7zip
+        apk update && apk add nano wget curl libguestfs p7zip guestfs-tools
     else
         echo "[FAILED] Unsupported distribution: $ID"
         exit 1
